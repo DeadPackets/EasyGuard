c0196110aade140b69b94ba9fb93ece9c95651bf
DeadPackets <b00073615@aus.edu>
2021-01-31 12:52:54 +0400

    Setting up

---
+++ app.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, session
+from flask_session.__init__ import Session
+from flask_talisman import Talisman, DENY
+from flask_seasurf import SeaSurf
+
+# Library imports
+from lib.DB import db
+
+# Initialize flask
+app = Flask(__name__)
+app.config.from_object('config')
+
+# Initialize Flask-Talisman for security headers
+csp = {
+	'default-src': '\'self\''
+}
+Talisman(app,
+	content_security_policy=csp, frame_options=DENY,
+	force_https=app.config['FORCE_HTTPS'],
+	strict_transport_security=app.config['FORCE_HTTPS'],
+	session_cookie_secure=app.config['SESSION_COOKIE_SECURE'])
+
+# Initialize Flask-SeaSurf for CSRF
+csrf = SeaSurf(app)
+
+# Initialize SQLAlchemy
+db.init_app(app)
+
+# Set up flask sessions
+Session(app)
---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+# Development
+DEVELOPMENT = False
+
+# Flask
+SECRET_KEY = os.urandom(32)
+
+# SQLAlchemy/Flask-SQLAlchemy
+SQLALCHEMY_DATABASE_URI = 'sqlite://easyguard.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_ECHO = False
+
+# Flask Session
+SESSION_COOKIE_NAME = 'session'
+SESSION_TYPE = 'sqlalchemy'
+SESSION_USE_SIGNER = True
+SESSION_COOKIE_HTTPONLY = True
+SESSION_COOKIE_SECURE = True
+
+# Security settings
+FORCE_HTTPS = True
+CSRF_COOKIE_HTTPONLY = True
+CSRF_COOKIE_SECURE = True
+
+# !!! LEAVE THIS SECTION ALONE !!!
+if os.environ['FLASK_ENV'] == 'development':
+    SQLALCHEMY_ECHO = True
+    DEVELOPMENT = True
+    FORCE_HTTPS = False
+    CSRF_COOKIE_SECURE = False
+    SESSION_COOKIE_SECURE = False
